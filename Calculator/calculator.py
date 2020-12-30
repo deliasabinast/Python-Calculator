de227c978e69e02d74c4153dbb0e74686e987094
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2020-12-30 17:52:06 +0200

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -21,17 +21,40 @@ tokens = [
     'FLOAT',
     'VAR'
 ]
-
+t_ignore = r' '
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_DIV = r'\/'
 t_MUL = r'\*'
 t_SQRT = r'\rad'
 t_POW = r'\^^'
+"""
 t_LOG = r'\log'
 t_SIN = r'\sin'
 t_COS = r'\cos'
 t_TG = r'\tan'
 t_CTG = r'\ctan'
+"""
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_VAR(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = 'VAR'
+    return t
+
+def t_error(t):
+    print("Syntax error")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
 
 
