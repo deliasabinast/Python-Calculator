38e288f6eada28e9023f9a6e73d5cb92006ce4df
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2021-01-04 12:45:21 +0200

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -10,32 +10,35 @@ tokens = [
     'MINUS',
     'DIV',
     'MUL',
-    'SQRT',
     'POW',
-    'LOG',
-    'SIN',
-    'COS',
-    'TG',
-    'CTG',
+    'FUNCTION',
     'INT',
     'FLOAT',
-    'VAR'
+    'VAR',
+    'LPAREN',
+    'RPAREN'
 ]
+
 t_ignore = r' '
+
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_DIV = r'\/'
 t_MUL = r'\*'
-t_SQRT = r'\rad'
 t_POW = r'\^^'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
 """
-t_LOG = r'\log'
+#t_LOG = r'\log'
 t_SIN = r'\sin'
 t_COS = r'\cos'
 t_TG = r'\tan'
-t_CTG = r'\ctan'
+#t_CTG = r'\ctan'
+t_SQRT = r'\rad'
 """
 
+"""
 def t_INT(t):
     r'\d+'
     t.value = int(t.value)
@@ -45,10 +48,20 @@ def t_FLOAT(t):
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
+    """
 
 def t_VAR(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = 'VAR'
+    return t
+
+def t_FUNCTION(t):
+    r'sin|cos|tan|log|rad'
+    return t
+
+def t_NUMBER(t):
+    r'([0-9]+\.[0-9]*)'
+    t.value = float(t.value)
     return t
 
 def t_error(t):
